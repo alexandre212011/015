1275bfd7c0bf149f33c6f88ab3b9b88ad01f2540
aleksandre212011 <aleksandro.khachidze@gmail.com>
2025-07-20 15:03:31 +0400

    commit 2

--- main.js
+++ main.js
@@ -18,3 +18,50 @@ fetch("https://dummyjson.com/products")
               document.getElementById("card-container").appendChild(productDiv);
           }
       });
+
+function showCart() {
+  const cart = JSON.parse(localStorage.getItem('cart')) || [];
+  const cartItems = document.getElementById('cart-items');
+  cartItems.innerHTML = '';
+
+  if (cart.length === 0) {
+    cartItems.innerHTML = '<li>Your cart is empty.</li>';
+    return;
+  }
+
+  cart.forEach(item => {
+    const li = document.createElement('li');
+    li.textContent = `${item.title} - $${item.price}`;
+    cartItems.appendChild(li);
+  });
+}
+
+
+function toggleCart() {
+  const cartSection = document.getElementById('cart-section');
+
+  if (cartSection.style.display === 'none') {
+    cartSection.style.display = 'block';
+    showCart(); 
+  } else {
+    cartSection.style.display = 'none';
+  }
+};
+
+const toggle = document.getElementById('toggleDark');
+const body = document.querySelector('body');
+
+toggle.addEventListener('click', function(){
+    this.classList.toggle('bi-moon');
+    if(this.classList.toggle('bi-brightness-high-fill')){
+        body.style.background = 'white';
+        body.style.color = 'black';
+        body.style.transition = '0.3s';
+    }else{
+        body.style.background = 'black';
+        body.style.color = 'white';
+        body.style.transition = '0.3s';
+    }
+      localStorage.setItem("mode", isDark ? "dark" : "light");
+});
+
